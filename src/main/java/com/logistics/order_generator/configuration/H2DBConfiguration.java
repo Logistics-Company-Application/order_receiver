3b9a33b1e3db8b68e4ca399ae9ebe0b2ec5d3b5e
JTCollins33 <JTCollinsy99@gmail.com>
2025-04-07 11:35:16 -0400

    correctly publishing to db

--- src/main/java/com/logistics/order_generator/configuration/H2DBConfiguration.java
+++ src/main/java/com/logistics/order_generator/configuration/H2DBConfiguration.java
@@ -17,7 +17,6 @@ public class H2DBConfiguration {
     public void initH2DB(){
         try{
             jdbcTemplate.execute("drop table orders if exists");
-//            jdbcTemplate.execute("create table employees(id serial,first_name varchar(255),last_name varchar(255))");
             jdbcTemplate.execute("create table orders(order_id INTEGER, product_name varchar(20), description varchar(20), destination varchar(20), buyer_name varchar(20), order_placed_timestamp timestamp)");
         } catch (Exception e){
             LOGGER.error("\n\nUnable to create schema for H2 database with error: \n\n", e);
--- src/main/java/com/logistics/order_generator/consumer/MessageConsumer.java
+++ src/main/java/com/logistics/order_generator/consumer/MessageConsumer.java
@@ -6,13 +6,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.logistics.order_generator.models.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
+import java.util.Map;
+
 @Component
 public class MessageConsumer {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
     private ObjectMapper objectMapper;
+    private static final String NEW_ORDER_SQL_INSERT_STATEMENT = """
+            insert into orders(order_id, product_name, description, destination, buyer_name, order_placed_timestamp)
+            values (?, ?, ?, ?, ?, ?)
+            """;
 
     public MessageConsumer(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
@@ -35,9 +47,27 @@ public class MessageConsumer {
         return newOrder;
     }
 
+    public void postNewOrderToDB(Order newOrder){
+        try{
+            jdbcTemplate.update(NEW_ORDER_SQL_INSERT_STATEMENT,
+                    newOrder.getOrderId(),
+                    newOrder.getProductName(),
+                    newOrder.getDescription(),
+                    newOrder.getDestination(),
+                    newOrder.getBuyerName(),
+                    newOrder.getOrderPlacedTimestamp()
+            );
+            LOGGER.info("Successfully published order {} to the H2 database", newOrder.getOrderId());
+        } catch (Exception e){
+            LOGGER.error("Encountered unexpected exception during attempted insert: ", e);
+            throw new RuntimeException();
+        }
+    }
+
     @KafkaListener(topics = "topic-1", groupId = "group-id")
     public void listenToTopic(String orderJsonString){
         Order newOrder = convertJsonStringToOrder(orderJsonString);
         LOGGER.info("\n\nReceived new order with id {}, productName of {}, description of {}, destination of {}, buyername of {} and placed at {}\n\n", newOrder.getOrderId(), newOrder.getProductName(), newOrder.getDescription(), newOrder.getDestination(), newOrder.getBuyerName(), newOrder.getOrderPlacedTimestamp());
+        postNewOrderToDB(newOrder);
     }
 }
